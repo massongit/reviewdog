47142f20bd4143243b6928b4af9bc2e03e4a58fe
Masaya Suzuki <suzukimasaya428@gmail.com>
2024-07-07 13:45:04 +0900

    fix

--- filter/diff_filter.go
+++ filter/diff_filter.go
@@ -128,13 +128,13 @@ func (df *DiffFilter) ShouldReport(path string, lnum int) (bool, *diff.FileDiff,
 	file := df.difffiles[npath]
 	lines, ok := df.difflines[npath]
 	if !ok {
-		fmt.Printf("%+v %+v\n", df.difflines, npath)
 		return df.mode == ModeNoFilter, file, nil
 	}
 	line, ok := lines[lnum]
 	if !ok {
 		return df.mode == ModeNoFilter || df.mode == ModeFile, file, nil
 	}
+	fmt.Printf("%+v %+v\n", line)
 	return df.isSignificantLine(line), file, line
 }
 
